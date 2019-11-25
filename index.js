ab340160ca62d2a1f48ea4a51ea2fd75f38122ad
Samuel Catley <scp94373@gmail.com>
2019-11-25 21:41:56 +0000

    update

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 const squareCollection = document.querySelector('.squareCollection');
+const fullX = document.querySelector('.X');
+const topVCollection = document.querySelector('.topVCollection');
+const bottomVCollection = document.querySelector('.bottomVCollection');
 //Prep for adding squares under squareCollection
-const fragment = document.createDocumentFragment();
+const topVFragment = document.createDocumentFragment();
+const bottomVFragment = document.createDocumentFragment();
+
 //number of squares
-const grid = [1, 20];
+const grid = [1, 10];
 //number of squares in column 0
 const col = grid[0];
 //number of squares in row 1
@@ -13,22 +18,28 @@ const numberOfElements = col * row;
 //Create <number> of squares to be under/within "squareCollection"
 for (let i = 0; i < numberOfElements; i++) {
   //create variable for new div so can be referenced 
-  const newDiv = document.createElement('div');
+  const bottomVDiv = document.createElement('div');
+  const topVDiv = document.createElement('div');
   //Add a class of square
-  newDiv.classList.add('square')
+  topVDiv.classList.add('topV')
+  bottomVDiv.classList.add('bottomV')
   //Add a class of square + number 
-  newDiv.classList.add('square'+ i)
+  bottomVDiv.classList.add('topV'+ i)
+  topVDiv.classList.add('topV'+ i)
   //add 'children' squares in variable 'fragment'
-  fragment.appendChild(newDiv);
+  topVFragment.appendChild(topVDiv);
+  bottomVFragment.appendChild(bottomVDiv);
 }
 //Add divs to squareCollection
-squareCollection.appendChild(fragment);
+topVCollection.appendChild(topVFragment);
+bottomVCollection.appendChild(bottomVFragment);
+fullX.appendChild(topVCollection);
+fullX.appendChild(bottomVCollection);
 //Creating a variable ref all 'square' classes
 const squares = document.querySelectorAll('.square');
+const topVDivs = document.querySelectorAll('.topV');
+const bottomVDivs = document.querySelectorAll('.bottomV');
 
-const minXPos = 0;
-let currentXPos = 0;
-const maxXPos = 30;
 
 // function wavyLine() {
 //   if(currentXPos >= maxXPos) {
@@ -43,15 +54,30 @@ const maxXPos = 30;
 // squares.forEach(square => square.style.left = `${wavyLine()}px`)
 
 //Default animation to 'setup' the structure of squares
+// anime({
+//   //target class of all squares
+//   targets: '.square',
+//   //do not loop
+//   loop: false,
+//   //'stagger' from 0 - 250 on the Y Axis
+//   translateY: anime.stagger([0, 250], {from: 'center'}),
+//   //Move on the X Axis
+//   translateX: anime.stagger([0, 100]),
+//   //Make all squares have a borderRadius of 40%
+//   borderRadius: ['0%', '40%'],
+//   //Change size of squares
+//   scaleY: 2,
+//   // delay: anime.stagger(100)
+// });
 anime({
   //target class of all squares
-  targets: '.square',
+  targets: ['.topV', '.bottomV'],
   //do not loop
   loop: false,
   //'stagger' from 0 - 250 on the Y Axis
-  translateY: anime.stagger([0, 250], {from: 'center'}),
+  translateY: anime.stagger([0, 500], {from: 'center'}),
   //Move on the X Axis
-  translateX: anime.stagger([0, 100]),
+  translateX: anime.stagger([0, 200]),
   //Make all squares have a borderRadius of 40%
   borderRadius: ['0%', '40%'],
   //Change size of squares
@@ -59,16 +85,50 @@ anime({
   // delay: anime.stagger(100)
 });
 
-//Bottom V
 anime({
   //target class of all squares
-  targets: '.squareCollection',
+  targets: '.topVCollection',
   //do not loop
   loop: false,
+  rotate: 180,
+  scale: 1.4
+});
+
+anime({
+  //target class of all squares
+  targets: '.bottomVCollection',
+  //do not loop
+  loop: false,
+  scale: 1.4
+});
+
+// anime({
+//   //target class of all squares
+//   targets: '.bottomV',
+//   //do not loop
+//   loop: false,
+//   //'stagger' from 0 - 250 on the Y Axis
+//   translateY: anime.stagger([0, 250], {from: 'center'}),
+//   //Move on the X Axis
+//   translateX: anime.stagger([0, 200]),
+//   //Make all squares have a borderRadius of 40%
+//   borderRadius: ['0%', '40%'],
+//   //Change size of squares
+//   scaleY: 2,
+//   // delay: anime.stagger(100)
+// });
+
+
+anime({
+  //target class of all squares
+  targets: '.X',
+  //do not loop
+  loop: false,
+  translateY: 800,
+  translateX: 300,
   //'stagger' from 0 - 250 on the Y Axis
-  translateY: 100,
   //Move on the X Axis
-  translateX: 100,
+
   //Make all squares have a borderRadius of 40%
   // borderRadius: ['0%', '40%'],
   //Change size of squares
@@ -78,7 +138,18 @@ anime({
 
 function pulse() {
   anime({
-    targets: '.square',
+    targets: '.topV',
+    scale: [
+      {value: 1.5, easing: 'easeInOutSine', duration: 200},
+      {value: 1, easing: 'easeInOutSine', duration: 200}
+    ],
+    loop: true,
+    // backgroundColor: '#ff0000',
+    // delay: anime.stagger(100, {from: 'center'})
+    delay: anime.stagger(100)
+  });
+  anime({
+    targets: '.bottomV',
     scale: [
       {value: 1.5, easing: 'easeInOutSine', duration: 200},
       {value: 1, easing: 'easeInOutSine', duration: 200}
@@ -94,7 +165,17 @@ function pulse() {
 
 function reversePulse() {
   anime({
-    targets: '.square',
+    targets: '.topV',
+    scale: [
+      {value: 2, easing: 'easeInOutSine', duration: 100},
+      {value: 1, easing: 'easeInOutSine', duration: 200}
+    ],
+    loop: false,
+    delay: anime.stagger(100, {from: 'center'})
+    // delay: anime.stagger(100)
+  });
+  anime({
+    targets: '.bottomV',
     scale: [
       {value: 2, easing: 'easeInOutSine', duration: 100},
       {value: 1, easing: 'easeInOutSine', duration: 200}
@@ -105,6 +186,8 @@ function reversePulse() {
   });
 }
 
-squareCollection.addEventListener('click', pulse)
-squareCollection.addEventListener('mouseout', reversePulse)
+topVCollection.addEventListener('click', pulse)
+bottomVCollection.addEventListener('click', pulse)
+topVCollection.addEventListener('mouseout', reversePulse)
+bottomVCollection.addEventListener('mouseout', reversePulse)
 
